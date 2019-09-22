56f77e77e3ecf183d4def3b6a7b3d8adb1e3ccf8
Eligijus <e.kiudys@gmail.com>
2019-09-22 13:40:58 +0300

    admin login
    created administrator login system with sessions

--- admin-login.php
+++ admin-login.php
@@ -1,4 +1,37 @@
 <?php
+
+include "dbh.inc";
+include 'header.inc';
+$username = $_POST["username"];
+$password = $_POST["password"];
+$cnt = 0;
+
+foreach ($_POST as $key => $value) {
+    if (isset($_POST['' . $key . '']) && $_POST['' . $key . ''] != " " && $_POST['' . $key . ''] != "") {
+        $cnt = $cnt + 1;
+    }
+}
+
+if($cnt === 2) {
+    $loginRequest = "SELECT * FROM ADMINS
+    WHERE admin_user = '$username' ";
+    if ($res = mysqli_query($sql, $loginRequest)) {
+
+        while ($row = mysqli_fetch_row($res)) {
+            if(password_verify ($password, $row[5]))
+            {
+                $_SESSION['username'] = $row[1];
+                $_SESSION['administrator'] = true;
+                header("Location: ./user.php");
+                exit;
+            }
+        }
+    }
+}
+else
+{
+    echo "Užpildykite visus laukus";
+}
 ?>
 
 <!DOCTYPE html>
--- admin.php
+++ admin.php
@@ -1,1 +1,10 @@
 <?php
+include 'header.inc';
+if(isset($_GET['LogOut']))
+{
+    $_SESSION['username'] = null;
+    $_SESSION['administrator'] = null;
+    header( "Location: ./admin-login.php");
+}
+
+?>
